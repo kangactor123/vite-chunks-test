24841ca91707a305dd0c89b34942614998be9f13
강동희 <dhkang@innogrid.com>
2025-08-13 14:08:56 +0900

    feat: component2 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 
 const ComponentOne = React.lazy(() => import("./component-one"));
+const ComponentTwo = React.lazy(() => import("./component-two"));
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       hello boy
@@ -13,9 +15,17 @@ function App() {
       >
         hello world
       </button>
+      <button onClick={() => setOpen((prev) => !prev)}>
+        click open Component2
+      </button>
       <Suspense fallback={<div>Loading...</div>}>
         <ComponentOne />
       </Suspense>
+      {open && (
+        <Suspense fallback={<div>Loading ComponentTwo...</div>}>
+          <ComponentTwo />
+        </Suspense>
+      )}
     </div>
   );
 }
---
+++ src/component-two.tsx
@@ -0,0 +1,16 @@
+const ComponentTwo = () => {
+  return (
+    <div>
+      ComponentTwo
+      <button
+        onClick={() => {
+          alert("ComponentTwo clicked!");
+        }}
+      >
+        click com2
+      </button>
+    </div>
+  );
+};
+
+export default ComponentTwo;
